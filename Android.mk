464a3119ad14cc8bcacd170ec9e3c16695204dd4
DionKill <118828188+DionKill@users.noreply.github.com>
2024-11-08 17:04:00 +0100

    Update Android.mk
    Changed something that i don't know, dude wtf am i doing

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter cherry che10,$(TARGET_DEVICE)),)
+ifneq ($(filter hwt1a21l T1-A21L,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
